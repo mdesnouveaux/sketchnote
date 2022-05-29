5cc42ec48aad53fc684c9d9dea0c0938aacb0c1d
mdesnouveaux <mathieu@desnouveaux.fr>
2022-05-30 00:09:15 +0200

    add image optimisation process

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,84 @@
+
+  const Image = require('@11ty/eleventy-img');
+  const outdent = require("outdent");
+
+  /** Maps a config of attribute-value pairs to an HTML string
+ * representing those same attribute-value pairs.
+ */
+const stringifyAttributes = (attributeMap) => {
+  return Object.entries(attributeMap)
+    .map(([attribute, value]) => {
+      if (typeof value === 'undefined') return '';
+      return `${attribute}="${value}"`;
+    })
+    .join(' ');
+};
+
+  const imageShortcode = async (
+    src,
+    alt,
+    className = undefined,
+    widths = [400, 800, 1280],
+    formats = ['webp', 'jpeg'],
+    sizes = '100vw'
+  ) => {
+    const imageMetadata = await Image(src, {
+      widths: [...widths, null],
+      formats: [...formats, null],
+      outputDir: 'dist/assets/images',
+      urlPath: 'assets/images',
+    });
+  
+    const sourceHtmlString = Object.values(imageMetadata)
+      // Map each format to the source HTML markup
+      .map((images) => {
+        // The first entry is representative of all the others
+        // since they each have the same shape
+        const { sourceType } = images[0];
+  
+        // Use our util from earlier to make our lives easier
+        const sourceAttributes = stringifyAttributes({
+          type: sourceType,
+          // srcset needs to be a comma-separated attribute
+          srcset: images.map((image) => image.srcset).join(', '),
+          sizes,
+        });
+  
+        // Return one <source> per format
+        return `<source ${sourceAttributes}>`;
+      })
+      .join('\n');
+  
+    const getLargestImage = (format) => {
+      const images = imageMetadata[format];
+      return images[images.length - 1];
+    }
+  
+    const largestUnoptimizedImg = getLargestImage(formats[0]);
+    const imgAttributes = stringifyAttributes({
+      src: largestUnoptimizedImg.url,
+      width: largestUnoptimizedImg.width,
+      height: largestUnoptimizedImg.height,
+      alt,
+      loading: 'lazy',
+      decoding: 'async',
+    });
+    const imgHtmlString = `<img ${imgAttributes}>`;
+  
+    const pictureAttributes = stringifyAttributes({
+      class: className,
+    });
+    const picture = `<picture ${pictureAttributes}>
+      ${sourceHtmlString}
+      ${imgHtmlString}
+    </picture>`;
+  
+    return outdent`${picture}`;
+  };
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
 
   // eleventyConfig.addPassthroughCopy("**/*.jpeg");
   // faster
@@ -13,6 +93,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
   // Return your Object options:
   return {
+    markdownTemplateEngine: "njk",
     dir: {
       input: 'pages',
       output: 'dist'
